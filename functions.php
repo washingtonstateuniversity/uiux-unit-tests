3b1cf50a54c865b8ed0f357367bd055bfbf884ab
Jeremy Bass <jeremybass@cableone.net>
2015-03-05 10:43:20 -0800

    remove unsed

--- functions.php
+++ functions.php
@@ -1,19 +1,4 @@
 <?php
-function extract_domain($domain){
-    if(preg_match("/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i", $domain, $matches)){
-        return $matches['domain'];
-    } else {
-        return $domain;
-    }
-}
-function extract_subdomains($domain){
-    $subdomains = $domain;
-    $domain = extract_domain($subdomains);
-
-    $subdomains = rtrim(strstr($subdomains, $domain, true), '.');
-
-    return $subdomains;
-}
 
 add_action( 'init', 'wsu_ga_remove_analytics' );
 function wsu_ga_remove_analytics() {
